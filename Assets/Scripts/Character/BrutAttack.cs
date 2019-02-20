b91f33403782e982fd5dabeda9dc9b6a234980a5
Geejob <geegame@outlook.fr>
2019-02-20 18:05:58 +0100

    update eventcontroller

---
+++ Assets/Scripts/Character/BrutAttack.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+
+    private Animator _componentAnimator;
+
+    private void Awake()
+     {
+        _componentAnimator = GetComponent<Animator>();
+     }
+
+    public void SimpleAttackAnimation()
+     {
+        _componentAnimator.SetBool("Attack", true);
+        _componentAnimator.SetBool("run", false);
+        _componentAnimator.SetBool("walk", false);
+     }
+
+}
--- Assets/Scripts/Character/CharacterEventsController.cs
+++ Assets/Scripts/Character/CharacterEventsController.cs
@@ -92,4 +92,5 @@ public class CharacterEventsController : MonoBehaviour
         _characterController.Jump();
         _animatorController.JumpAnimation();
     }
+
 }
