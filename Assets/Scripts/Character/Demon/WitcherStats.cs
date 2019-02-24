cea17a82ff2b57911b74823946981b422327d50c
Geejob <geegame@outlook.fr>
2019-02-24 09:37:48 +0100

    add paladin model and animations

---
+++ Assets/Scripts/Character/Demon/WitcherStats.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WitcherStats : MonoBehaviour
+{
+    [SerializeField] private int _speedRun = 6;
+    [SerializeField] private int _speedWalk = 3;
+    [SerializeField] private int _jumpForce = 100;
+
+    private CharacterController _characterController;
+
+    private void Start()
+    {
+        _characterController = GetComponent<CharacterController>();
+
+        if (_characterController == null) return;
+
+        _characterController.SpeedRun = _speedRun;
+        _characterController.SpeedWalk = _speedWalk;
+        _characterController.JumpForce = _jumpForce;
+
+        if (_characterController.IsRunnig)
+        {
+            _characterController.InitSpeed(_speedRun);
+        }
+        else
+        {
+            _characterController.InitSpeed(_speedWalk);
+        }
+    }
+}
---
+++ Assets/Scripts/Character/Paladin/PaladinEventsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaladinEventsController : MonoBehaviour
+{
+    private PaladinAnimatorController _PaladinAnimator;
+
+    private void Start()
+    {
+        _PaladinAnimator = GetComponent<PaladinAnimatorController>();
+    }
+
+    private void Update()
+    {
+        MouseEvents();
+    }
+
+    private void AnimationWeakAttack()
+    {
+        _PaladinAnimator.WeaksAttacks();
+    }
+
+    private void MouseEvents()
+    {
+        if (transform.tag == "Player Character")
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                AnimationWeakAttack();
+            }
+        }
+    }
+}
+
