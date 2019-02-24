c549c3d0e1aaf948de5a587774b68ef0bdcc173e
Marvinosaure <marvin.polette@gmail.com>
2019-02-24 01:10:05 +0100

    add script nav mesh agent

---
+++ Assets/Scripts/Character/Brut/BrutNavMeshAgent.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BrutNavMeshAgent : MonoBehaviour
+{
+    [SerializeField] float _attackDistance = 1f;
+
+    private NavMeshAgent _agent;
+    private BrutAnimatorController _brutAnimator;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _brutAnimator = GetComponent<BrutAnimatorController>();
+    }
+    
+    private void Update()
+    {
+        if (gameObject.tag == "Player Character") return;
+
+        if (_agent.remainingDistance < _attackDistance)
+        {
+            _agent.speed = 0;
+            _brutAnimator.WeaksAttacks();
+        }        
+    }
+}
---
+++ Assets/Scripts/IA/SimpleIAScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SimpleIAScript : MonoBehaviour
+{
+    [SerializeField] float _runDistance = 20f;    
+    [SerializeField] float _securityDistance = 1f;
+
+    private NavMeshAgent _agent;
+    private Transform _target;
+    private AnimatorController _animator;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _target = GameObject.Find("Player").GetComponent<Transform>();
+        _animator = GetComponent<AnimatorController>();
+    }
+    
+    private void Update()
+    {
+        if (gameObject.tag == "Player Character") return;
+
+        if(_agent.remainingDistance > _runDistance)
+        {
+            _agent.speed = 0;
+            _animator.IdleAnimation();
+        }
+        else if(_agent.remainingDistance > _securityDistance)
+        {
+            _agent.speed = 3;
+            _animator.RunAnimation();
+        }
+        else
+        {
+            _animator.IdleAnimation();
+            _agent.speed = 0;
+        }
+
+        _agent.SetDestination(_target.position);
+    }
+}
